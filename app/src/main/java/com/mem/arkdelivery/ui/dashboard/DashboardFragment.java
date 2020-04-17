c54e6b5e6a22ff28e2edbd188d8155bdaaa06358
MrPonerYea-mem <63256783+MrPonerYea-mem@users.noreply.github.com>
2020-04-17 17:17:03 +0300

    Добавлена интерефес заказво

--- app/src/main/java/com/mem/arkdelivery/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/mem/arkdelivery/ui/dashboard/DashboardFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,20 +16,35 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.mem.arkdelivery.R;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DashboardFragment extends Fragment {
 
     private DashboardViewModel dashboardViewModel;
+    private Spinner spinnerCity1;
+    private Spinner spinnerCity2;
+    String[] city = {"Москва 1 пункт", "Санкт-Петербург пунтк 1", "Архангельск пункт 1", "Москва 2 пункт", "Санкт-Петербург пунтк 2", "Архангельск пункт 2"};
+    Map<String, Integer[]> position = new HashMap<>();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         dashboardViewModel =
                 ViewModelProviders.of(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+
         final TextView textView = root.findViewById(R.id.text_dashboard);
+        spinnerCity1 = (Spinner) root.findViewById(R.id.spinner);
+        spinnerCity2 = (Spinner) root.findViewById(R.id.spinner2);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_item, city);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerCity1.setAdapter(adapter);
+        spinnerCity2.setAdapter(adapter);
+
         dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+                //textView.setText(s);
             }
         });
         return root;
